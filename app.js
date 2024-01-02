269426c636c3913bea6ac40dce34e4481ce24b54
Seth Hobson <sethlawrencehobson@gmail.com>
2024-01-01 20:25:49 -0500

    added showfunction

--- app.js
+++ app.js
@@ -14,17 +14,62 @@ async function showNumberTrivia() {
 
   const number = prompt();
 
-  const resp = await fetch(`${BASE_API}/${number}/trivia`, {
-    method: "POST",
-    body: JSON.stringify({}),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
-
-  // const resp = await fetch("http://numbersapi.com/45/trivia");
+  const resp = await fetch(`${BASE_API}/${number}/trivia?json`)
 
   const trivia = await resp.json();
 
   console.log(trivia);
 }
+
+// Have a “race”: make a new function, showNumberRace, that asks for trivia about four
+//different numbers (using four separate requests), but, as soon as one request returns,
+// log the piece of trivia for the winning number to the console.
+
+
+/**
+ * asks for trivia about four
+//different numbers (using four separate requests), but, as soon as one request returns,
+// log the piece of trivia for the winning number to the console.
+ */
+async function showNumberRace() {
+
+  const r1 = fetch(`${BASE_API}/random/trivia?json`);
+  const r2 = fetch(`${BASE_API}/random/trivia?json`);
+  const r3 = fetch(`${BASE_API}/random/trivia?json`);
+  const r4 = fetch(`${BASE_API}/random/trivia?json`);
+
+  const response = await Promise.race([r1,r2,r3,r4]);
+
+  const answer = await response.json();
+
+  console.log(answer.text);
+}
+
+// showNumberRace();
+
+
+/**Get all: make a new function, showNumberAll.
+ * that asks for trivia about about different numbers.
+ *  Make all of the requests at the same time,
+ * but handle them once all requests are completed. */
+
+async function showNumberAll() {
+
+  const r1 = fetch(`${BASE_API}/random/trivia?json`);
+  const r2 = fetch(`${BASE_API}/random/trivia?json`);
+  const r3 = fetch(`${BASE_API}/random/trivia?json`);
+  const r4 = fetch(`${BASE_API}/WRONG`);
+
+  const response = await Promise.allSettled([r1,r2,r3,r4]);
+
+
+
+
+
+  console.log(response);
+  return response;
+}
+
+showNumberAll();
+
+
